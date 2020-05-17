e564929eb6b4ba9a55df71ff5d7fe5cd2b35ceda
Pluficorn <n.verkade@st.hanze.nl>
2020-05-17 15:25:38 +0200

    1.5 Start, veld toegevoegd

--- src/main/java/KassaRij.java
+++ src/main/java/KassaRij.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class KassaRij {
+    private ArrayList<Dienblad> dienbladen;
 
     /**
      * Constructor
      */
     public KassaRij() {
-        // method body omitted
+        this.dienbladen = new ArrayList<>();
     }
 
     /**
@@ -16,7 +17,7 @@ public class KassaRij {
      * @param klant
      */
     public void sluitAchteraan(Dienblad klant) {
-        // method body omitted
+
     }
 
     /**
@@ -26,7 +27,7 @@ public class KassaRij {
      * @return Eerste klant in de rij of null
      */
     public Dienblad eerstePersoonInRij() {
-        // method body omitted
+
     }
 
     /**
@@ -35,6 +36,6 @@ public class KassaRij {
      * @return Of er wel of geen rij bestaat
      */
     public boolean erIsEenRij() {
-        // method body omitted
+
     }
 }
