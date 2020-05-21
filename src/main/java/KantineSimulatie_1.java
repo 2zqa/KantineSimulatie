01ef8ce7b31b2e6053b72044fcdc9d15de5ef171
2zqa <25235249+2zqa@users.noreply.github.com>
2020-05-21 20:06:09 +0200

    2.2 Implementeer methodes

--- src/main/java/KantineSimulatie_1.java
+++ src/main/java/KantineSimulatie_1.java
@@ -15,26 +15,27 @@ public class KantineSimulatie {
      * Deze methode simuleert een aantal dagen in het
      * verloop van de kantine
      *
-     * @param dagen
+     * @param dagen aantal dagen dat gesimuleerd moet worden
      */
     public void simuleer(int dagen) {
 
         // herhaal voor elke dag
-        for (int i = 0; ...) {
+        for (int i = 0; i< DAGEN; i++) {
 
             // per dag nu even vast 10 + i personen naar binnen
             // laten gaan, wordt volgende week veranderd...
 
             // for lus voor personen
             for (int j = 0; j < 10 + i; j++) {
-                // kantine.(...);
+                kantine.loopPakSluitAan();
             }
 
             // verwerk rij voor de kassa
 
             // toon dagtotalen (artikelen en geld in kassa)
-
+            System.out.println("Aantal artikelen: " + kantine.aantalArtikelen() + "\nHoeveelheid geld in de kassa: " + kantine.hoeveelheidGeldInKassa());
             // reset de kassa voor de volgende dag
+            kantine.resetKassa();
         }
     }
 
